c92a7148339e9cefc1bffc7b9dac42cfb28919c3
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2026-07-02 19:43:19 +0530

    Initialize Frontend , Backend Done!

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import WebSocket, { WebSocketServer } from "ws";
+import { GameManager } from "./GameManager.js";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const gameManager = new GameManager();
+
+wss.on("connection", function connection(ws: WebSocket) {
+  gameManager.addUser(ws);
+  ws.on("close", () => {
+    gameManager.removeUser(ws);
+  });
+});
